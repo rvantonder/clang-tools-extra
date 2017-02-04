b88da8b0f8b92f55e951371b792a52cee46c1bd5
Rijnard van Tonder <rvantonder@gmail.com>
2017-02-03 19:21:14 -0500

    tutorial for finding for loop statements with variable initialized to 0

--- tutorial/Tutorial.cpp
+++ tutorial/Tutorial.cpp
@@ -5,8 +5,30 @@
 // Declares llvm::cl::extrahelp.
 #include "llvm/Support/CommandLine.h"
 
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+
 using namespace clang::tooling;
 using namespace llvm;
+
+using namespace clang;
+using namespace clang::ast_matchers;
+
+// This is a thing that matches. If it matches, bind it to an identifier
+StatementMatcher LoopMatcher =
+  forStmt(hasLoopInit(declStmt(hasSingleDecl(varDecl(
+    hasInitializer(integerLiteral(equals(0)))))))).bind("forLoop");
+
+// Printer is a callback over matching nodes
+class LoopPrinter : public MatchFinder::MatchCallback {
+public :
+  virtual void run(const MatchFinder::MatchResult &Result) {
+    // refer to the identifier for matching nodes
+    // cast result to a "ForStmt" object
+    if (const ForStmt *FS = Result.Nodes.getNodeAs<clang::ForStmt>("forLoop"))
+      FS->dump(); // dumps the ENTIRE for statement
+  }
+};
 
 // Apply a custom category to all command-line options so that they are the
 // only ones displayed.
@@ -24,5 +46,13 @@ int main(int argc, const char **argv) {
   CommonOptionsParser OptionsParser(argc, argv, TutorialOptionCategory);
   ClangTool Tool(OptionsParser.getCompilations(),
                  OptionsParser.getSourcePathList());
-  return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>().get());
+
+
+  // printer is a callback over matching nodes
+  LoopPrinter Printer;
+  // initialize a new finder
+  MatchFinder Finder;
+  Finder.addMatcher(LoopMatcher, &Printer);
+
+  return Tool.run(newFrontendActionFactory(&Finder).get());
 }
